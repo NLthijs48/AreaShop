5c9fdb6cc92822ecd31cdcca05166fd97daa7a91
Thijs Wiefferink <nlthijs48@gmail.com>
2015-02-09 23:46:32 +0100

    Added name change detection, updates regions when name is changed.
    When a player logs in it will loop through regions owned by this players
    and check if the saved playername is still the same as the current
    playername and update the regionflags and -signs if necessary. Also
    fixed some spelling mistakes in the English language file and updated to
    the latest Transifex translations.

--- src/nl/evolutioncoding/areashop/listeners/PlayerLoginListener.java
+++ src/nl/evolutioncoding/areashop/listeners/PlayerLoginListener.java
@@ -1,6 +1,10 @@
 package nl.evolutioncoding.areashop.listeners;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import nl.evolutioncoding.areashop.AreaShop;
+import nl.evolutioncoding.areashop.regions.GeneralRegion;
 import nl.evolutioncoding.areashop.regions.RentRegion;
 
 import org.bukkit.entity.Player;
@@ -65,6 +69,41 @@ public final class PlayerLoginListener implements Listener {
 				}
 	        }.runTaskLater(plugin, 20);			
 		}
+		// Check if the player has regions that use an old name of him and update them
+		final List<GeneralRegion> regions = new ArrayList<GeneralRegion>(plugin.getFileManager().getRegions());
+		new BukkitRunnable() {
+			private int current = 0;
+			
+			@Override
+			public void run() {
+				for(int i=0; i<plugin.getConfig().getInt("nameupdate.regionsPerTick"); i++) {
+					if(current < regions.size()) {
+						GeneralRegion region = regions.get(current);
+						if(region.isOwner(player)) {
+							if(region.isBuyRegion()) {
+								if(!player.getName().equals(region.getStringSetting("buy.buyerName"))) {
+									region.setSetting("buy.buyerName", player.getName());
+									region.saveRequired();
+									region.updateRegionFlags();
+									region.updateSigns();
+								}
+							} else if(region.isRentRegion()) {
+								if(!player.getName().equals(region.getStringSetting("rent.renterName"))) {
+									region.setSetting("rent.renterName", player.getName());
+									region.saveRequired();
+									region.updateRegionFlags();
+									region.updateSigns();
+								}
+							}
+						}
+						current++;
+					}
+				}
+				if(current >= regions.size()) {
+					this.cancel();
+				}
+			}
+		}.runTaskTimer(plugin, 22, 1); // Wait a bit before starting to prevent a lot of stress on the server when a player joins (a lot of plugins already do stuff then)
 	}
 }
 

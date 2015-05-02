791b696aff83334904876cbd71ee637fc05f8529
Thijs Wiefferink <nlthijs48@gmail.com>
2015-05-03 00:15:05 +0200

    Add support for Metric prefixes to display large numbers
    Indications are used as defined on:
    http://en.wikipedia.org/wiki/Metric_prefix, implemented from 'k' to 'Y'.
    Added setting 'metricSuffixesAbove' to config.yml to specify from which
    number these characters should be used.
    Added setting 'decimalMark' to change the character(s) used as decimal
    separation (is a comma instead of a dot in certain countries).

--- AreaShop/src/main/java/nl/evolutioncoding/areashop/AreaShop.java
+++ AreaShop/src/main/java/nl/evolutioncoding/areashop/AreaShop.java
@@ -579,17 +579,52 @@ public final class AreaShop extends JavaPlugin implements AreaShopInterface {
 		before = before.replace(currencyEuro, "\u20ac");
 		String after = this.getConfig().getString("moneyCharacterAfter");
 		after = after.replace(currencyEuro, "\u20ac");
-		BigDecimal bigDecimal = new BigDecimal(amount);
-	    bigDecimal = bigDecimal.setScale(getConfig().getInt("fractionalNumbers"), RoundingMode.HALF_UP);
-	    if(bigDecimal.doubleValue() > 1000000000) {
-	    	bigDecimal = bigDecimal.stripTrailingZeros();
-	    }
-	    double newAmount = bigDecimal.doubleValue();
-		if(getConfig().getBoolean("hideEmptyFractionalPart") && (newAmount%1.0) == 0.0) {
-			return before + bigDecimal.toString().replace(".0", "") + after;
+	    String result;
+		// Add metric 
+		double metricAbove = getConfig().getDouble("metricSuffixesAbove");
+		if(metricAbove != -1 && amount >= metricAbove) {
+			if(amount >= 1000000000000000000000000.0) {
+				amount = amount/1000000000000000000000000.0;
+				after = "Y" + after;
+			} else if(amount >= 1000000000000000000000.0) {
+				amount = amount/1000000000000000000000.0;
+				after = "Z" + after;
+			} else if(amount >= 1000000000000000000.0) {
+				amount = amount/1000000000000000000.0;
+				after = "E" + after;
+			} else if(amount >= 1000000000000000.0) {
+				amount = amount/1000000000000000.0;
+				after = "P" + after;
+			} else if(amount >= 1000000000000.0) {
+				amount = amount/1000000000000.0;
+				after = "T" + after;
+			} else if(amount >= 1000000000.0) {
+				amount = amount/1000000000.0;
+				after = "G" + after;
+			} else if(amount >= 1000000.0) {
+				amount = amount/1000000.0;
+				after = "M" + after;
+			} else if(amount >= 1000.0) {
+				amount = amount/1000.0;
+				after = "k" + after;
+			}
+			BigDecimal bigDecimal = new BigDecimal(amount);
+			bigDecimal.setScale(getConfig().getInt("fractionalNumbers"), RoundingMode.HALF_UP);
+			if(bigDecimal.toString().contains(".")) {
+				int frontLength = bigDecimal.toString().substring(0, bigDecimal.toString().indexOf('.')).length();
+			    bigDecimal = bigDecimal.setScale(getConfig().getInt("fractionalNumbers") + (3-frontLength), RoundingMode.HALF_UP);
+			}
+		    result = bigDecimal.toString();
 		} else {
-			return before + bigDecimal.toString() + after;
+			BigDecimal bigDecimal = new BigDecimal(amount);
+			bigDecimal.setScale(getConfig().getInt("fractionalNumbers"), RoundingMode.HALF_UP);
+		    result = bigDecimal.toString();
+			if(getConfig().getBoolean("hideEmptyFractionalPart") && (amount%1.0) == 0.0 && result.contains(".")) {
+				result = result.substring(0, result.indexOf('.'));
+			}
 		}
+		result = result.replace(".", getConfig().getString("decimalMark"));
+		return before + result + after;
 	}	
 	
 	/**
